12c1882c3cf40d1ce4a34f32d1da4d447e5f25b9
A. Kasim Acar <68561632+Dethian@users.noreply.github.com>
2021-06-24 22:23:58 +0200

    calculator-with-python
    calculator with python
    
    twitter.com/menthord

---
+++ README.md
@@ -0,0 +1,3 @@
+Calculator
+
+Calculator with Python
---
+++ Statistics/Statistic.py
@@ -0,0 +1,8 @@
+from Calculator.Calculator import Calculator
+from Statistics.Mean import mean
+
+class Statistics(Calculator):
+
+    def mean(self, data):
+        self.result = mean(data)
+        return self.result
